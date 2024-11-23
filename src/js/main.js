208f7272095191a3fabf9f569c859f734379a139
Oleg Sazonov <oleg.sazonov.work@gmail.com>
2024-11-23 22:51:03 +0200

    feat(playVideo.js): add VideoPlayer using YouTube Player API

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,12 @@
 'use strict';
 
 import Slider from "./modules/slider";
+import VideoPlayer from "./modules/playVideo";
 
 window.addEventListener('DOMContentLoaded', () => {
 	const slider = new Slider('.page', '.next');
 	slider.render();
+
+	const player = new VideoPlayer('.showup .play', '.overlay');
+	player.init();
 });
---
+++ src/js/modules/playVideo.js
@@ -0,0 +1,66 @@
+'use strict';
+
+export default class VideoPlayer {
+	constructor(triggers, overlay){
+		this.buttons = document.querySelectorAll(triggers);
+		this.overlay = document.querySelector(overlay);
+		this.close = this.overlay.querySelector('.close');
+	}
+
+	bindTriggers() {
+		this.buttons.forEach(btn => {
+			btn.addEventListener('click', () => {
+				if (document.querySelector('iframe#frame')) {
+					this.overlay.style.display = 'flex';
+				} else {
+					const path = btn.getAttribute('data-url');
+				
+					this.createPlayer(path);
+					this.overlay.classList.remove('animate__fadeOut');
+					this.overlay.classList.add('animate__fadeIn');
+				}
+			});
+		});
+	}
+
+	bindCloseTriggers() {
+		[this.close, this.overlay].forEach(trigger => {
+			trigger.addEventListener('click', e => {
+				if (e.target === this.overlay || this.close) {
+					this.overlay.style.display = 'none';
+					this.player.stopVideo();
+				}
+			});
+		});
+
+		// window.addEventListener('keydown', (e) => {
+		// 	if (e.key === 'Escape' || e.code === 'Escape') {
+		// 		e.stopImmediatePropagation();
+		// 		this.overlay.style.display = 'none';
+		// 		this.player.stopVideo();
+		// 	}
+		// });
+	}
+
+	createPlayer(url) {
+		this.player = new YT.Player('frame', {
+			height: '100%',
+			width: '100%',
+			videoId: `${url}`
+		});
+
+		console.log(this.player);
+		this.overlay.style.display = 'flex';
+	}
+
+	init() {
+		const tag = document.createElement('script');
+
+		tag.src = "https://www.youtube.com/iframe_api";
+		const firstScriptTag = document.getElementsByTagName('script')[0];
+		firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+		this.bindTriggers();
+		this.bindCloseTriggers();
+	}
+}
