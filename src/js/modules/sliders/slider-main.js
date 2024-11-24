b4ab30f4d45942a8fde9cffaa5cad9020f093f0f
Oleg Sazonov <oleg.sazonov.work@gmail.com>
2024-11-24 20:47:49 +0200

    refactor(slider/slider-main.js): add abstract-slider

--- src/js/modules/sliders/slider-main.js
+++ src/js/modules/sliders/slider-main.js
@@ -1,11 +1,10 @@
-'use strict';
+'use sctrict';
 
-export default class Slider {
-	constructor(page, btns){
-		this.page = document.querySelector(page);
-		this.slides = [...this.page.children];
-		this.btns = document.querySelectorAll(btns);
-		this.slideIndex = 1;
+import Slider from "./slider";
+
+export default class MainSlider extends Slider {
+	constructor(page, btns) {
+		super(page, btns);
 	}
 
 	showSlides(n) {
