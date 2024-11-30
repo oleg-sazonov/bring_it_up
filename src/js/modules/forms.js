612c738618b37a802b3389b587347a659168edc2
Oleg Sazonov <oleg.sazonov.work@gmail.com>
2024-11-30 23:18:05 +0200

    feat(forms.js): ad validation into forms

--- src/js/modules/forms.js
+++ src/js/modules/forms.js
@@ -19,51 +19,69 @@ export default class Form {
 		});
 	}
 
+	validateInput(input, form, message) {
+        if (input.value.trim() === '' || (input.id === 'phone' && input.value.length < 3)) {
+			input.style.border = '1px solid red'; 
+			form.parentNode.append(message);
+            return false;
+        } else {
+			input.style.border = ''; 
+			message.textContent = '';
+            return true;
+        }
+    };
+
 	init() {
 		initPhoneMask();
 		this.checkMailInputs();
+
 		this.forms.forEach(form => {
-			// const submitButton = form.querySelector('button[type="submit"]');
 
-			// if (form.querySelector('status')) {
-			// 	form.parentNode.remove(form.querySelector('status'));
-			// }
-
-			// let statusMessage = document.createElement('div');
-			// statusMessage.classList.add('status');
-			// statusMessage.style.cssText = `
-			// margin-top: 15px;
-			// font-size: 18px;
-			// color: grey;
-			// `;
+			const inputs = Array.from(form.querySelectorAll('input'));
+			let statusMessage = document.createElement('div');
+			statusMessage.style.cssText = `
+				margin-top: 15px;
+				font-size: 18px;
+				color: grey;
+			`;
         
-			// form.addEventListener('input', () => {
-			// 	const inputs = Array.from(form.querySelectorAll('input'));
-			// 	const allFilled = inputs.every(input => input.value.trim() !== '');
-	
-			// 	submitButton.disabled = !allFilled;
-			// });
-
 			form.addEventListener('submit', e => {
 				e.preventDefault();
 
-				form.parentNode.append(handleStatus('loading'));
+				let allValid = true;
 
-				const formData = new FormData(form);
-				postData(this.path, formData)
-					.then(res => {
-						console.log(res);
-						handleStatus('idle');
-					})
-					.catch(() => {
-						handleStatus('error');
-					})
-					.finally(() => {
-						form.reset();
-						setTimeout(() => {
-							handleStatus().remove();
-						}, 6000);
-					})
+				inputs.forEach(input => {
+					if (!this.validateInput(input, form, statusMessage)) {
+						allValid = false; 
+					}
+				});
+
+				// Real-time validation
+				inputs.forEach(input => {
+					input.addEventListener('input', () => this.validateInput(input, form, statusMessage));
+				});
+
+				if (allValid) {
+					form.parentNode.append(handleStatus('loading'));
+
+					const formData = new FormData(form);
+					postData(this.path, formData)
+						.then(res => {
+							console.log(res);
+							handleStatus('idle');
+						})
+						.catch(() => {
+							handleStatus('error');
+						})
+						.finally(() => {
+							form.reset();
+							setTimeout(() => {
+								handleStatus().remove();
+							}, 6000);
+						})
+				} else {
+					statusMessage.textContent = 'Please fill out all inputs.';
+				}
 			});
 		});
 	}
