f34ccfeec8585c7dd48c81db85ce896a2784ce9c
Oleg Sazonov <oleg.sazonov.work@gmail.com>
2024-12-07 17:17:52 +0200

    feat(download.js): add download links

--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,7 @@ import VideoPlayer from "./modules/playVideo";
 import Difference from "./modules/difference";
 import Form from "./modules/forms";
 import ShowInfo from "./modules/showInfo";
+import Download from "./modules/download";
 
 window.addEventListener('DOMContentLoaded', () => {
 	const mainSlider = new MainSlider({container: '.page', btns: '.next'});
@@ -51,4 +52,5 @@ window.addEventListener('DOMContentLoaded', () => {
 	new Difference('.officerold', '.officernew', '.officer__card-item').init();
 	new Form('.form').init();
 	new ShowInfo('.plus__content').init();
+	new Download('.download').init();
 });
